a0a55cbdbcbd259765d1aab1557aeec6871ead30
Hugues <hugues.desaxce@orange.com>
2014-11-06 18:03:40 +0100

    Added verbose to script

--- nb_request/build.sh
+++ nb_request/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+set -o verbose
 
 #Create the tiles of images
-for i in $(seq 1 5);
+for i in $(seq 1 100);
 do
 convert leopard.jpg -crop `python -c "print 100.0/$i"`%x0 $i-%d.jpg;
 done
 
 # Create the html files
-for i in $(seq 1 5);
+for i in $(seq 1 100);
 do
 sed "s/?/$i/g" x_request.html > $i.html;
 done
